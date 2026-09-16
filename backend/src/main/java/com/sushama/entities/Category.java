715e95bc9e73dfb02d4f394ee69b80279eb14ccf
Sushama <sushama121@gmail.com>
2026-09-16 16:37:49 +0530

    Fix targetEntity for subCategories in Category

--- backend/src/main/java/com/sushama/entities/Category.java
+++ backend/src/main/java/com/sushama/entities/Category.java
@@ -1,28 +1,21 @@
 package com.sushama.entities;
 
 import java.util.List;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Table(name = "categories")
 @Data
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Category {
 
     @Id
@@ -30,14 +23,19 @@ public class Category {
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @Size(min = 3, max = 50, message = "Category must be 3-50 characters")
     private String name;
 
+    @Column(length = 500)
     private String description;
 
-    @Column(name = "status")
+    @Column(nullable = false)
     private String status = "ACTIVE";
 
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
-    @JsonIgnore
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY, targetEntity = SubCategory.class)
+    @JsonIgnoreProperties("category")
     private List subCategories;
+
+    @Transient
+    private int productCount;
 }
