e9bfd70899e5c167cb2a88afc1ba0f7648ec92e9
Sushama <sushama121@gmail.com>
2026-09-16 13:01:12 +0530

    Add explicit Lombok Getter and Setter annotations to SubCategory

--- backend/src/main/java/com/sushama/entities/SubCategory.java
+++ backend/src/main/java/com/sushama/entities/SubCategory.java
@@ -7,15 +7,26 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "subcategories")
 @Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubCategory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false)
     private String name;
